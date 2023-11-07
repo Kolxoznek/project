6871ff3aa64b808bb7fe5851f02690746275fd9e
Evgeniy <defender35185@gmail.com>
2023-11-07 17:59:56 +0300

    улучшена самая первая задача с фильмами

--- main.js
+++ main.js
@@ -123,6 +123,7 @@
     // thirdTask()
     
 
+    /* ======================= вывод пирамиды из звездочек в консоль ======================= */
 
     // function figure() {
     //     const star = '*'
@@ -153,10 +154,61 @@
 //     }
 //     console.log(result)
 
+/* ============================================================================================ */
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "")
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+let i = 0
+while (i < 2) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+          
+          (a != null && b != null && a != "" && b != "" && a.length <= 50) ?
+            personalMovieDB.movies[a] = b : i--;
+        i++;
+}
+
+      if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов")
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель")
+    } else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман")
+    } else console.log("Произошла ошибка")
+
+    console.log(personalMovieDB)
 
 
 
+    // for ( let i = 0; i < 2; i++) {
+    //     const a = prompt('Один из последних просмотренных фильмов?', ''),
+    //           b = prompt('На сколько оцените его?', '')
+    //     if (a != null && b != null && a != "" && b != "" && a.length <= 50) {
+    //         personalMovieDB.movies[a] = b
 
+    //     } else {
+    //         i--
+    //     }
+    //   }
 
-
-
+    // let i = 0
+    // while (i < 2) {
+    //     const a = prompt('Один из последних просмотренных фильмов?', ''),
+    //           b = prompt('На сколько оцените его?', '')
+    //           if (a != null && b != null && a != "" && b != "" && a.length <= 50) {
+    //             personalMovieDB.movies[a] = b
+    
+    //         } else {
+    //             i--
+    //         }
+    //         i++
+    // }
+    
